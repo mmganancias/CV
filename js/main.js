0de6a7fb0acbc07d3c6891a504c169e64c518e65
marcelamachucaganancias <address>
2023-06-09 19:58:55 -0300

    steet alert

--- js/main.js
+++ js/main.js
@@ -8,4 +8,33 @@ function validacion(e) {
 }
 
 let formulario = e.target
+
 */
+
+const btn = document.querySelector("#myBtn")
+
+btn.addEventListener('click', function () {
+
+    Swal.fire({
+        title: 'Muchas gracias!',
+        text: 'Te enviaremos todas nuestras novedades!',
+    })
+})
+
+const usuario = document.querySelector('#')
+
+usuario.addEventListener('click', () => {
+    const { value: email } = await Swal.fire({
+        title: 'Input email address',
+        input: 'email',
+        inputLabel: 'Your email address',
+        inputPlaceholder: 'Enter your email address'
+      })
+      
+      if (email) {
+        Swal.fire(`Entered email: ${email}`)
+      }
+
+})
+
+
